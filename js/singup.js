dbc36a33dc8bac8971fa1d49c9c62adee5b1954b
Chevesade Ornano <chevesade.ornano@utp.ac.pa>
2026-01-30 14:56:01 -0500

    PAGINA PERFIL

---
+++ js/singup.js
@@ -0,0 +1,12 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+	container.classList.remove("right-panel-active");
+});
+// Added event listeners to toggle between sign up and sign in views
