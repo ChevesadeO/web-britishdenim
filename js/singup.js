9dff9438df57f984a9d8e924df27457a00da8653
Chevesade Ornano <166187418+ChevesadeO@users.noreply.github.com>
2026-03-02 22:15:30 -0500

    se cambio la pocicion del logo de estar centreado a la izquierda de la web

--- js/singup.js
+++ js/singup.js
@@ -1,12 +1,13 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
+document.addEventListener('DOMContentLoaded', () => {
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
-signUpButton.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
-});
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
 
-signInButton.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
 });
-// Added event listeners to toggle between sign up and sign in views
