36dc3241b10eb23efdd8a4379b464a76cd9b5fc1
Chevesade Ornano <chevesade.ornano@utp.ac.pa>
2026-03-04 16:47:31 -0500

    cambios en pagina de inicio

--- js/header.js
+++ js/header.js
@@ -1,10 +1,9 @@
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".main-header");
 
-  const header = document.querySelector('.main-header');
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 60) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
