303875ca0354fcd6728f66dd9b5651e1c321109c
Chevesade Ornano <chevesade.ornano@utp.ac.pa>
2026-03-16 10:44:59 -0500

    refactor: reorganize URL patterns for improved readability and remove unused JavaScript file

--- britishdenim/core/urls.py
+++ britishdenim/core/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('perfil/', views.perfil, name='perfil'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('regiprod/', views.regiprod, name='regiprod'),
+
+path("", views.index, name="index"),
+
+path("perfil/", views.perfil, name="perfil"),
+
+path("dashboard/", views.dashboard, name="dashboard"),
+
+path("registro-producto/", views.regiprod, name="regiprod"),
+
+path("contacto/", views.contacto, name="contacto"),
+
 ]
